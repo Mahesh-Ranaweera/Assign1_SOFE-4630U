db6008035af8cab203aaf852d2c15ec23ef23c0d
Mahesh Ranaweera <mahesh.ranaweerakaluarachchige@uoit.net>
2018-01-23 17:35:32 -0500

    working on the postgres

--- app/db.js
+++ app/db.js
@@ -1,3 +1,28 @@
 /**
  * Contain the connection to the mongo database
  */
+
+ const {
+     Client
+ } = require('pg')
+
+ const client = new Client({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'webapp',
+    password: 'a2fb22ce710899f6e455b3984ee16930',
+    post: 5432,
+});
+
+client.connect();
+
+client.on('error', (err, client)=>{
+    console.error('Unexpected error on idle client', err);
+    process.exit(-1);
+});
+
+const query = await client.query(
+    'CREATE TABLE IF NOT EXISTS users(id SERIAL PRIMARY KEY, fname VARCHAR(100), lname VARCHAR(100))'
+);
+
+await client.end();
