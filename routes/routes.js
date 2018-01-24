889dcbdead13ae7701e56084aa7434411fb45524
Mahesh Ranaweera <mahesh.ranaweerakaluarachchige@uoit.net>
2018-01-24 18:41:30 -0500

    working on layout

--- routes/routes.js
+++ routes/routes.js
@@ -7,4 +7,14 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/**GET Signin Page */
+router.get('/signin', function(req, res, next){
+  res.render('signin', { title: 'Signin' });
+});
+
+/**GET Signup Page */
+router.get('/signup', function(req, res, next){
+  res.render('signup', { title: 'Signup' });
+});
+
 module.exports = router;
