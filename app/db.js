6a0b74075821437960eac0cf2866266858c266ef
Mahesh Ranaweera <mi6softlab@gmail.com>
2018-01-27 20:04:35 -0500

    able to create display notes

--- app/db.js
+++ app/db.js
@@ -2,11 +2,11 @@
  * Contain the connection to the mongo database
  */
 
- const {
-     Client
- } = require('pg')
+const {
+    Client
+} = require('pg')
 
- const client = new Client({
+const client = new Client({
     user: 'postgres',
     host: 'localhost',
     database: 'webapp',
@@ -16,74 +16,127 @@
 
 client.connect();
 
-client.on('error', (err, client)=>{
+client.on('error', (err, client) => {
     console.error('Unexpected error on idle client', err);
     process.exit(-1);
 });
 
-const createTB = 
-    'CREATE TABLE IF NOT EXISTS users('+
-    'email VARCHAR(100) PRIMARY KEY,'+
-    'fname VARCHAR(100) NOT NULL,'+
-    'lname VARCHAR(100) NOT NULL,'+
-    'passw VARCHAR(100) NOT NULL);'+
-    'CREATE TABLE IF NOT EXISTS notes('+
-    'id SERIAL PRIMARY KEY,'+
-    'email VARCHAR(100) NOT NULL,'+
-    'mime_type VARCHAR(255) NOT NULL,'+
-    'file_name VARCHAR(255) NOT NULL,'+
-    'file_data BYTEA NOT NULL);';
+const createTB =
+    'CREATE TABLE IF NOT EXISTS users(' +
+    'email VARCHAR(100) PRIMARY KEY,' +
+    'fname VARCHAR(100) NOT NULL,' +
+    'lname VARCHAR(100) NOT NULL,' +
+    'passw VARCHAR(100) NOT NULL);' +
+    'CREATE TABLE IF NOT EXISTS notes(' +
+    'id SERIAL PRIMARY KEY,' +
+    'email VARCHAR(100) NOT NULL,' +
+    'heading VARCHAR(200) NOT NULL,' +
+    'subhead VARCHAR(200) NOT NULL,' +
+    'content TEXT NOT NULL);'
 
 client.query(createTB, (err, res) => {
-    if(err){
+    if (err) {
         console.log(err.stack);
-    }else{
+    } else {
         console.log("|| TABLES CREATED ||")
     }
 });
 
-var addUSER = function(data, callback){
-    
+/**Add new user to db */
+var addUSER = function(data, callback) {
+
     var sql = "INSERT INTO users(email, fname, lname, passw) values($1, $2, $3, $4)";
     var values = [data.email, data.fname, data.lname, data.passw];
 
     client.query(sql, values, (err, res) => {
-        if(err){
+        if (err) {
             //console.log(err);
-            
+
             //duplicate KEY
-            if(err.code == 23505){
+            if (err.code == 23505) {
                 callback(-1);
-            }else{
+            } else {
                 callback(0);
             }
-        }else{
+        } else {
             //console.log(res);
             callback(1);
         }
     });
 }
 
-var getUSER = function(useremail, callback){
-    
+/**Get user data for signin */
+var getUSER = function(useremail, callback) {
+
     var sql = "SELECT email, fname, lname, passw FROM users WHERE email=$1";
     var values = [useremail];
 
     client.query(sql, values, (err, res) => {
-        if(err){
+        if (err) {
             //console.log(err);
             callback(err);
-        }else{
-            if(res.rows.length == 0){
+        } else {
+            if (res.rows.length == 0) {
                 callback(null);
-            }else{
+            } else {
                 callback(res.rows);
             }
         }
     });
 }
 
+/**Add notes */
+var addNote = function(data, callback) {
+
+    var sql = "INSERT INTO notes(email, heading, subhead, content) values($1, $2, $3, $4)";
+    var values = [data.email, data.head, data.subhead, data.content];
+
+    client.query(sql, values, (err, res) => {
+        if (err) {
+            callback(0);
+        } else {
+            callback(1);
+        }
+    });
+}
+
+/**GET notes from db */
+var getNotes = function(useremail, callback) {
+
+    var sql = "SELECT id, email, heading, subhead, content FROM notes WHERE email=$1 ORDER BY id DESC";
+    var values = [useremail];
+
+    client.query(sql, values, (err, res) => {
+        if (err) {
+            console.log(err);
+            callback(err);
+        } else {
+            if (res.rows.length == 0) {
+                callback(null);
+            } else {
+                callback(res.rows);
+            }
+        }
+    });
+}
+
+/**Delete note */
+var deleteNote = function(id, callback) {
+    var sql = "SELECT FROM notes WHERE id=$1";
+    var values = [id];
+
+    client.query(sql, values, (err, res) => {
+        if (err) {
+            callback(0);
+        } else {
+            callback(1);
+        }
+    });
+}
 
 /**Export the modules */
 module.exports.addUSER = addUSER;
 module.exports.getUSER = getUSER;
+module.exports.addNote = addNote;
+module.exports.getNotes = getNotes;
+module.exports.deleteNote = deleteNote;
--- routes/routes.js
+++ routes/routes.js
@@ -1,167 +1,219 @@
-
 var express = require('express');
 var router = express.Router();
 var dbconn = require('../app/db');
-var encrypt= require('../app/encrypt');
+var encrypt = require('../app/encrypt');
 var session = require('express-session');
 
 //set the session
 router.use(session({
-  secret: 'HV3U00lcMahc84050VxX62xoMS67NhS4',
-  resave: false,
-  saveUninitialized: true,
-  path: '/'
+    secret: 'HV3U00lcMahc84050VxX62xoMS67NhS4',
+    resave: false,
+    saveUninitialized: true,
+    path: '/'
 }));
 
 //start a session
 var sess;
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
-  /**Redirect user if session exists */
-  if(req.session.usersess){
-    res.redirect('/dashboard');
-  }else{
-    res.render('index', {
-      title: 'Notebook'
-    });
-  }
+router.get('/', function(req, res, next) {
+    /**Redirect user if session exists */
+    if (req.session.usersess) {
+        res.redirect('/dashboard');
+    } else {
+        res.render('index', {
+            title: 'Notebook'
+        });
+    }
 });
 
 /**GET Signin Page */
-router.get('/signin', function (req, res, next) {
-  var alert = null;
+router.get('/signin', function(req, res, next) {
+    var alert = null;
 
-  if(req.query.notify != null){
-    alert = req.query.notify;
-  }
+    if (req.query.notify != null) {
+        alert = req.query.notify;
+    }
 
-  res.render('signin', {
-    title: 'Notebook:signin',
-    alert: alert
-  });
+    res.render('signin', {
+        title: 'Notebook:signin',
+        alert: alert
+    });
 });
 
 /**POST Signin */
-router.post('/signin', function (req, res, next) {
-  var email = req.body.strEmail;
-  var passw = req.body.strPassw;
+router.post('/signin', function(req, res, next) {
+    var email = req.body.strEmail;
+    var passw = req.body.strPassw;
 
-  dbconn.getUSER(email, function(state){
+    dbconn.getUSER(email, function(state) {
 
-    /**JSON Objects */
-    user_data = JSON.stringify(state);
+        /**JSON Objects */
+        user_data = JSON.stringify(state);
 
-    //check if user exists
-    if(state != null){
-      user_data = JSON.parse(user_data);
+        //check if user exists
+        if (state != null) {
+            user_data = JSON.parse(user_data);
 
-      //Makesure email is correct and password is correct
-      if(user_data[0].email == email && encrypt.compareHASH(passw, user_data[0].passw)){
-        /**Create a session for the user */
-        sess = req.session;
-        sess.usersess = true;
-        sess.email = user_data[0].email;
-        sess.name  = user_data[0].fname + ' ' + user_data[0].lname;
+            //Makesure email is correct and password is correct
+            if (user_data[0].email == email && encrypt.compareHASH(passw, user_data[0].passw)) {
+                /**Create a session for the user */
+                sess = req.session;
+                sess.usersess = true;
+                sess.email = user_data[0].email;
+                sess.name = user_data[0].fname + ' ' + user_data[0].lname;
 
-        /**User Authenticated */
-        res.redirect('/dashboard');
-      }else{
-        /**Password Error */
-        res.redirect('/signin?notify=passw');
-      }
-    }else{
-      /**User not found */
-      res.redirect('/signin?notify=notfound');
-    }
-  });
+                /**User Authenticated */
+                res.redirect('/dashboard');
+            } else {
+                /**Password Error */
+                res.redirect('/signin?notify=passw');
+            }
+        } else {
+            /**User not found */
+            res.redirect('/signin?notify=notfound');
+        }
+    });
 });
 
 /**GET Signup Page */
-router.get('/signup', function (req, res, next) {
-  var alert = null;
+router.get('/signup', function(req, res, next) {
+    var alert = null;
 
-  if(req.query.notify != null){
-    alert = req.query.notify;
-  }
+    if (req.query.notify != null) {
+        alert = req.query.notify;
+    }
 
-  res.render('signup', {
-    title: 'Notebook:signup',
-    alert: alert
-  });
+    res.render('signup', {
+        title: 'Notebook:signup',
+        alert: alert
+    });
 });
 
 /**POST Student Signup */
-router.post('/signup', function (req, res, next) {
+router.post('/signup', function(req, res, next) {
 
-  var email = req.body.strEmail.toLowerCase();
-  var fname = req.body.strFname;
-  var lname = req.body.strLname;
+    var email = req.body.strEmail.toLowerCase();
+    var fname = req.body.strFname;
+    var lname = req.body.strLname;
 
-  var passw1 = req.body.strPassw1;
-  var passw2 = req.body.strPassw2;
+    var passw1 = req.body.strPassw1;
+    var passw2 = req.body.strPassw2;
 
-  if (passw1 == passw2) {
-    data = {
-      email: email,
-      fname: fname,
-      lname: lname,
-      passw: encrypt.passwHASH(passw1)
+    if (passw1 == passw2) {
+        data = {
+            email: email,
+            fname: fname,
+            lname: lname,
+            passw: encrypt.passwHASH(passw1)
+        }
+
+        //console.log(data);
+        /**Add the data to db */
+        dbconn.addUSER(data, function(state) {
+            if (state == 1) {
+                //console.log('Entered');
+                res.redirect('/signup?notify=success');
+            } else if (state == -1) {
+                //console.log('Duplicate');
+                res.redirect('/signup?notify=duplicate');
+            } else {
+                //console.log('Error');
+                res.redirect('/signup?notify=error')
+            }
+        });
+
+    } else {
+        res.redirect('/signup?notify=passw');
     }
-
-    //console.log(data);
-    /**Add the data to db */
-    dbconn.addUSER(data, function(state){
-      if(state == 1){
-        //console.log('Entered');
-        res.redirect('/signup?notify=success');
-      }else if(state == -1){
-        //console.log('Duplicate');
-        res.redirect('/signup?notify=duplicate');
-      }else{
-        //console.log('Error');
-        res.redirect('/signup?notify=error')
-      }
-    });
-
-  } else {
-    res.redirect('/signup?notify=passw');
-  }
 });
 
 /**GET Student Dashboard */
-router.get('/dashboard', function(req, res, next){
+router.get('/dashboard', function(req, res, next) {
 
-  /**Makesure user session exists */
-  if(req.session.usersess){
-    username = sess.name;
-    useremail= sess.email;
+    /**Makesure user session exists */
+    if (req.session.usersess) {
+        username = sess.name;
+        useremail = sess.email;
 
-    res.render('dashboard', {
-      title: 'Dashboard',
-      name: username
-    });
-  }else{
-    res.redirect('/');
-  }
+        /**GET users notes */
+        dbconn.getNotes(useremail, function(state) {
+
+            /**GET JSON data */
+            notes_data = JSON.stringify(state);
+            notes_data = JSON.parse(notes_data);
+            //console.log(notes_data);
+
+            res.render('dashboard', {
+                title: 'Dashboard',
+                name: username,
+                notes: notes_data
+            });
+        });
+    } else {
+        res.redirect('/');
+    }
 });
 
 /**GET new notes */
-router.get('/newnotes', function(req, res, next){
-  res.render('newnotes', {
-    title: 'Dashboard'
-  });
+router.get('/newnotes', function(req, res, next) {
+
+    /**Makesure user session exists */
+    if (req.session.usersess) {
+        username = sess.name;
+        useremail = sess.email;
+
+        res.render('newnotes', {
+            title: 'Dashboard',
+            head: null,
+            subhead: null,
+            content: null
+        });
+    } else {
+        res.redirect('/');
+    }
 });
 
-/**SIGNOUT */
-router.get('/signout', function(req, res, next){
-  req.session.destroy(function(err){
-    if(err){
-      console.log(err);
-    }else{
-      res.redirect('/');
+/**SAVE note */
+router.post('/savenote', function(req, res, next) {
+    /**Makesure user session exists */
+    if (req.session.usersess) {
+        data = {
+            email: sess.email,
+            head: req.body.strHead,
+            subhead: req.body.strSubhead,
+            content: req.body.strContent
+        }
+
+        //console.log(data);
+        dbconn.addNote(data, function(state) {
+            if (state == 1) {
+                res.redirect('/dashboard');
+            } else {
+                res.render('newnotes', {
+                    title: 'Dashboard',
+                    head: data.head,
+                    subhead: data.subhead,
+                    content: data.content,
+                    alert: 'error'
+                });
+            }
+        });
+    } else {
+        res.redirect('/');
     }
-  });
-}); 
+});
+
+
+/**SIGNOUT */
+router.get('/signout', function(req, res, next) {
+    req.session.destroy(function(err) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.redirect('/');
+        }
+    });
+});
 
 module.exports = router;
