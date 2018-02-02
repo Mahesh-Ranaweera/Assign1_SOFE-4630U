94780f8ba4d47ce99593aa3340fe3360238c66da
Mahesh Ranaweera <mi6softlab@gmail.com>
2018-02-02 12:28:12 -0500

    working on reading files

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,12 @@
-/**Resize the note window */
+/**read and uplaod file content*/
+function uploadFile() {
+    var targetF = event.target;
+
+    var f_reader = new FileReader();
+    f_reader.onload = function() {
+        var content = f_reader.result;
+        var textarea = document.getElementById('inputdiv').value = content;
+        console.log(content);
+    }
+    f_reader.readAsText(targetF.files[0]);
+}
