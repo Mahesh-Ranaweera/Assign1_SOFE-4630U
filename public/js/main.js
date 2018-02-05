724d270cbd9679d273672d346f2dc2910e9fa1b9
Mahesh Ranaweera <mi6softlab@gmail.com>
2018-02-05 13:27:57 -0500

    able read and insert value from a txt file

--- public/js/main.js
+++ public/js/main.js
@@ -1,14 +1,17 @@
 /**read and uplaod file content*/
-var uploadFile = function(event) {
-    var targetF = event.target.files[0];
+function uploadFile() {
+    var f_input = document.getElementById('fileinput');
+
+    var targetF = f_input.files[0];
 
     if (targetF) {
         var f_reader = new FileReader();
         f_reader.onload = function() {
             var content = f_reader.result;
-            var textarea = document.getElementById('inputdiv');
-            textarea.value = content;
-            console.log(content);
+
+            /**Update the editor with new data read from the file */
+            CKEDITOR.instances['inputdiv'].setData(content);
+            //console.log(content);
         }
         f_reader.readAsText(targetF);
     }
